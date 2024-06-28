0322749a2cdad89d2ba08525689dc46ccccd0c91
sajood ali <sajoodali486@gmail.com>
2024-06-28 20:58:59 +0500

    first commit

---
+++ 2 hello world server/server.mjs
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! i am express server')
+})
+
+app.listen(port, () => {
+  console.log(`hello server is listening on port ${port}`)
+})
